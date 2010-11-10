8128a9249474f4c2b0cce2ef2c6a2298551fe4f2
Max Taldykin <jorpic@gmail.com>
2010-11-10 19:13:57 +0300

    cpdt: p3_4 solved

--- cpdt/p3_4.v
+++ cpdt/p3_4.v
@@ -5,31 +5,31 @@ Inductive nat_tree: Set :=
   | Node: (nat -> nat_tree) -> nat_tree.
 
 
-Fixpoint increment (tree : nat_tree) :=
+Fixpoint increment (tree: nat_tree) :=
   match tree with
     | Leaf n  => Leaf (n + 1)
     | Node ns => Node (fun i => increment (ns i))
   end.
 
-Fixpoint leapfrog (i : nat) (tree : nat_tree) :=
+Fixpoint leapfrog (i: nat) (tree: nat_tree) :=
   match tree with
     | Leaf n  => n
     | Node ns => leapfrog (i+1) (ns i)
   end.
 
 
-Theorem inc_frog : forall (i : nat) (tree : nat_tree),
+Theorem inc_frog: forall (tree: nat_tree) (i: nat),
   leapfrog i tree + 1 = leapfrog i (increment tree).
 Proof.
-  intros.
-  induction tree.
+  intro t.
+  induction t.
    simpl; reflexivity.
 
-   unfold increment. fold increment.
-   unfold leapfrog. fold leapfrog.
+   unfold increment, leapfrog. fold increment. fold leapfrog.
+   intros.
+   apply (H i (i+1)).
+Qed.
 
-   Check (H (i+1)).
-   generalize (i+1).
 
 
   

4196b34e9487299491017bd0027de3853c1741df
max taldykin <jorpic@gmail.com>
2011-02-24 23:30:18 +0300

    basys2: glowing led

--- vhdl/ex1/led_glow.vhd
+++ vhdl/ex1/led_glow.vhd
@@ -1,4 +1,4 @@
-
+	
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -12,7 +12,9 @@ entity led_glow is port
 end led_glow;
 
 architecture Behavioral of led_glow is
-	signal divCounter: std_logic_vector (23 downto 0);
+	signal divCounter: std_logic_vector (25 downto 0);
+	signal pwmLevel: std_logic_vector (5 downto 0);
+	signal pwm: std_logic_vector (6 downto 0);
 begin
 	clkDiv: process (clk)
 	begin
@@ -21,6 +23,24 @@ begin
 		end if;
 	end process;
 
-	led <= divCounter (23); 
+	setLevel: process (clk)
+	begin
+		if clk'event and clk = '1'  then
+			if divCounter (25) = '1' then
+				pwmLevel <= divCounter (24 downto 19);
+			else
+				pwmLevel <= not divCounter (24 downto 19);
+			end if;
+		end if;
+	end process;
+	
+	pwmLoop: process (clk, pwmLevel)
+	begin
+		if clk'event and clk = '1'  then
+			pwm <= pwm + pwmLevel;
+		end if;
+	end process;
+		
+	led <= pwm (6); 
 end Behavioral;
 
